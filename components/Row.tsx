6214091b72d73b85022d36d74c82de763395dfcb
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-25 00:54:15 -0700

    debugged row api functionality

--- components/Row.tsx
+++ components/Row.tsx
@@ -15,17 +15,17 @@ function Row({ title, movies }: Props) {
       </h2>
       <div className="group relative md:-ml-2">
         <ChevronLeftIcon
-          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 `}
+          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition group-hover:opacity-100 hover:scale-125 `}
         />
 
-        <div className="scrollbar-hide flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2">
-          {movies.map((movie) => {
+        <div className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2">
+          {movies.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
-          })}
+          ))}
         </div>
 
         <ChevronRightIcon
-          className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`}
+          className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition group-hover:opacity-100 hover:scale-125`}
         />
       </div>
     </div>
